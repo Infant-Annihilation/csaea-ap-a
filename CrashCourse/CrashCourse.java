953001a382d100d225c9edc4aeafa93976397f0f
Infant-Annihilation <gabecpayton@gmail.com>
2026-09-03 13:38:24 +0000

    day 2 of java crash course

--- CrashCourse/CrashCourse.java
+++ CrashCourse/CrashCourse.java
@@ -1,5 +1,5 @@
 package CrashCourse;
-public class CrashCourse {        //class header
+public class CrashCourse {        //class header. Always PascalCase
 
     public static void main(String[] args) {
 
@@ -59,8 +59,47 @@ public class CrashCourse {        //class header
         System.out.println(e || f); // true
         System.out.println(e || f && !e); // true
 
-        System.out.println( e && f); // short circuits after checking e; java doesn't need to check after e
+        System.out.println(e && f); // short circuits after checking e; java doesn't need to check after e
+        System.out.println(f || e && f); // short circuits after checking the first t.
 
+        // CASTING(converting)
+        int g = (int)5.99;
+        System.out.println(g);
+
+        double h = (double)5/6;
+        System.out.println(h);
+
+        // STRINGS
+        String s1 = "Goodnight";
+        String s2 = " and ";
+        String s3 = "goodbye";
+        String result = (s1 + s2 + s3);
+        result += ", Cowboy.";
+        System.out.println("\n" + result + "\n");
+
+        // ARRAYS
+        int[] arry1 = new int[10];
+        System.out.println(arry1);
+        System.out.println("length: " + arry1.length);
+
+        arry1[0] = 11;      // assigning values to the individual integers in the array1 array
+        arry1[1] = 2;
+
+        System.out.println("Remainder: " + arry1[0] % arry1[1]);
+
+        int[] arry2 = {34, 52, 3, 64, 73};
+        System.out.println("arry2: " + arry2);
+
+        // 2D ARRAYS(grid or table)
+
+        int[][] arryGrid1 = new int [4][3];   // [rows][columns]
+        // 0 0 0
+        // 0 0 0
+        // 0 0 0
+        // 0 0 0
+
+        System.out.println("Rows: " + arryGrid1.length);
+        System.out.println("Columns: " + arryGrid1[0].length);
 
 
     }
