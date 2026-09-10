6a74984e3e9388e48bd23ec2000b0eb7560e75e8
Infant-Annihilation <gabecpayton@gmail.com>
2026-09-10 13:08:35 +0000

    basics of OOP

---
+++ BasicClass.java
@@ -0,0 +1,18 @@
+public class BasicClass {
+    public static void main(String[] args) {
+
+        // attributes / data
+        // variables
+
+        // Constructor --- Special method called every time you make an object
+        // same name as the class, followed by parenthesis.
+        // BasicClass()
+
+        // behaviors / methods
+        // you can write as many as you want
+
+
+
+    }
+
+}
