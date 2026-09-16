485adf5405f937b33883098bbec40dec0e5e7679
Infant-Annihilation <gabecpayton@gmail.com>
2026-09-16 14:28:51 +0000

    sneaker class

---
+++ CrashCourse/Sneaker.java
@@ -0,0 +1,53 @@
+package CrashCourse;
+
+public class Sneaker {
+    private String brand;
+    private String model;
+    private double size;
+    private String color;
+    private double price;
+    private int releaseYear;
+    private String material;
+    private boolean isLimitedEdition;
+    private boolean isSold;
+    private int amountInStock;
+
+
+    public Sneaker(String brand, String model, double size) { 
+        this.brand = brand;
+        this.model = model;
+        this.size = size;
+
+        isSold = false;
+        isLimitedEdition = false;
+
+    }
+
+    public void changeColor(String newColor) {
+        color = newColor;
+
+    }
+
+    public void changeSize(double newSize) {
+        size = newSize;
+
+    }
+
+    public void changePrice(double newPrice) {
+        price = newPrice;
+
+    }
+
+    public void sell() {
+        isSold = true;
+
+    }
+
+    public void displayInfo() {
+        System.out.println("Brand: " + brand + "\n" + "Model: " + model + "\n" + "Size: " + size + "\n" + "Color: " + color + "\n" + "Price: " + price + "\n" + "Release year: " + releaseYear + "\n" + "Material: " + material + "\n" + "Limited Edition status: " + isLimitedEdition + "\n" + "Sell status: " + isSold + "\n" + "Amount in stock: " + amountInStock + "\n");
+    }
+
+    public void receiveShipment() {
+        amountInStock += 3;
+    }
+}
